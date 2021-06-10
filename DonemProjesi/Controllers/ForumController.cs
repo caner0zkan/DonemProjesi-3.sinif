20a70c1225871b91154d967ef6b9a82adb777fee
M. Caner Özkan <canerozkan513@gmail.com>
2021-06-10 10:49:11 +0300

    iletisim

--- DonemProjesi/Controllers/ForumController.cs
+++ DonemProjesi/Controllers/ForumController.cs
@@ -15,10 +15,14 @@ namespace DonemProjesi.Controllers
         // GET: Forum
         public ActionResult Index()
         {
-            
-
             //Include(o => o.Ogrenci) -- öğrenci bilgilerini dahil ediyoruz
             return View(db.Projeler.Include(o => o.Ogrenci).ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string dsd)
+        {
+            return View();
         }
     }
 }
